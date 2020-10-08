4d729789a94fde227cac49e7de43fd3721e63861
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2020-10-08 20:56:26 +0530

    Create 1420A.cpp

---
+++ 1420A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[1000000+5];
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        for(int i = 0; i<n;i++){
+            cin >> a[i];
+        }
+        bool can = false;
+
+        for(int i = 1; i< n; i++){
+            if(a[i] >=a[i-1]){
+                can = true;
+                break;
+            }
+        }
+        if(can) cout << "YES" << "\n";
+        else cout << "NO" << "\n";
+    }
+
+    return 0;
+}
